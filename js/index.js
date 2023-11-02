357cb41c9321881e6944c8d5692185e9f71d1686
dev14sinka <106219633+dev14sinka@users.noreply.github.com>
2023-11-02 10:24:26 +0700

    Update to github

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
-var i = 0;
-var noiDung = 'KPR is a brand that focuses on collective narrative and empowering storytellers. Keepers is a living story, an uncharted world waiting to be explored, to be imagined.';
-var tocDo = 50;
+// var i = 0;
+// var noiDung = 'KPR is a brand that focuses on collective narrative and empowering storytellers. Keepers is a living story, an uncharted world waiting to be explored, to be imagined.';
+// var tocDo = 50;
 
-function hieuUngDanhChu() {
-  if (i < noiDung.length) {
-    document.getElementById("text_screen_project").innerHTML += noiDung.charAt(i);
-    i++;
-    setTimeout(hieuUngDanhChu, tocDo);
-  }
-}
+// function hieuUngDanhChu() {
+//   if (i < noiDung.length) {
+//     document.getElementById("text_screen_project").innerHTML += noiDung.charAt(i);
+//     i++;
+//     setTimeout(hieuUngDanhChu, tocDo);
+//   }
+// }
 
-hieuUngDanhChu()
+// hieuUngDanhChu()
+
+let parallax1 = document.getElementById('parallax1')
+let parallax2 = document.getElementById('parallax2')
+
+// window.addEventListener('scroll', () => {
+//   let value = window.scrollY
+//   console.log(parallax1.style.top);
+//   if (value === 0) {
+//     parallax1.style.top = 666 * 2.5 + 'px'
+//     parallax2.style.top = 769 * 2.5 + 'px'
+//   }
+//   else {
+
+//   }
+//   console.log(value);
+//   // parallax1.style.top = -value + 50 + 666 + 'px'
+//   // parallax2.style.top = -value + 50 + 769 + 'px'
+// })
